0d167ae4ef83e979aba1ac9fac23108bb729a139
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-06-14 19:00:32 -0300

    Add fizzbizz.c resolving Q1

--- Q1/fizzbizz.c
+++ Q1/fizzbizz.c
@@ -7,23 +7,54 @@
  * Output: utilize a função printf da biblioteca stdio.h e escreva um número ou palavra por linha (\n):
  * 
  */
+
+#define FIZZ_NUMBER 3
+#define BUZZ_NUMBER 5
+
+char _checkFizz(int number)
+{
+    return !(number % FIZZ_NUMBER);
+}
+
+char _checkBuzz(int number)
+{
+    return !(number % BUZZ_NUMBER);
+}
+
 void fizzBizz(int start, int end)
 {
     printf("\nFizzBizz::START: %d, %d\n", start, end);
-    
+
     // TODO: Your code here!
     // Ex.: printf("Fizz\n");
-    
+    int x;
+    char isFizz, isBuzz;
+    for (x = start; x < end; x++)
+    {
+        isFizz = _checkFizz(x);
+        isBuzz = _checkBuzz(x);
+        if (!isFizz && !isBuzz)
+            printf("%d", x);
+        else
+        {
+            if (isFizz)
+                printf("Fizz");
+            if (isBuzz)
+                printf("Buzz");
+        }
+        printf("\n");
+    }
+
     printf("\nFizzBizz::END\n");
 }
 
 int main()
 {
     printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: FizzBizz ###\n");
-    
+
     fizzBizz(1, 6);
     fizzBizz(1, 16);
     fizzBizz(1, 100);
-    
+
     return 0;
 }
